80fc57eeefde0157be01cb9956c35a42ac2897cb
rishi1215 <122065899+rishi1215@users.noreply.github.com>
2023-04-27 02:11:31 +0530

    Add files via upload

---
+++ AMcount.py
@@ -0,0 +1,17 @@
+def AMcount():
+   z = open("poem.txt", "w")
+   x = z.write("function to count a and m  in a strmMing including both the cases")
+   z.close()
+   op = open("poem.txt", "r")
+   y = op.read()
+   op.close()
+   countA = 0
+   countM = 0
+   for i in y:
+       if i in "aA" :
+           countA+=1
+       elif i in "mM" :
+           countM+=1
+   print("A or a = ", countA)
+   print("M or m = ", countM)
+AMcount()

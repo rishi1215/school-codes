778b51e1b6304e0ded470a014094564173494b39
rishi1215 <122065899+rishi1215@users.noreply.github.com>
2023-04-28 09:59:39 +0530

    Update AMcount.py

--- AMcount.py
+++ AMcount.py
@@ -1,10 +1,10 @@
 def AMcount():
    z = open("poem.txt", "w")
-   x = z.write("function to count a and m a in a strmMing including both the cases")
+   x = z.write("function to count a aand m a in a strmMing including both the cases")
    z.close()
-   op = open("poem.txt", "r")
-   y = op.read()
-   op.close()
+   p = open("poem.txt", "r")
+   y = p.read()
+   p.close()
    countA, countM = 0,0 
    for i in y:
        if i.isalpha():

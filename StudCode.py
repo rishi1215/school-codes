5c99c73ba5cf450278f1dcb099dfea69aa576b28
rishi1215 <122065899+rishi1215@users.noreply.github.com>
2023-05-18 03:10:14 +0530

    Add files via upload

---
+++ StudCode.py
@@ -0,0 +1,19 @@
+import pickle
+with open("stud.dat", "wb") as f:
+    ans = "y"
+    while ans.lower() == "y":
+        sid = int(input("Enter Student ID: "))
+        name = input("Enter Name of Student : ")
+        perc = int(input("Enter the Percentage :"))
+        marks = int(input("Enter the Total Marks : "))
+        dic = {"ID": sid, "Name": name, "Percentage": perc, "Total": marks}
+        pickle.dump(dic, f)
+        ans = input("Add more?y/n ")
+with open("stud.dat", "rb") as f:
+    try:
+        while True:
+            r = pickle.load(f)
+            if r["Percentage"] > 50:
+                print(r)
+    except:
+        pass

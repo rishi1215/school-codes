4fbd20cda11964da742f85ffc301062ab6a7857e
rishi1215 <122065899+rishi1215@users.noreply.github.com>
2023-05-18 05:33:56 +0530

    Update EmmpRP.py

--- EmmpRP.py
+++ EmmpRP.py
@@ -1,18 +1,20 @@
 import pickle
-with open("stud.dat", "wb") as f:
+with open("emp.dat", "wb") as f:
     ans = "y"
     while ans.lower() == "y":
-        eid = int(input("Enter Student ID: "))
-        name = input("Enter Name of Student : ")
-        city = int(input("Enter the City :"))
-        dic = {"ID": eid, "Name": name, "City": city}
+        eid = int(input("Enter Employee ID: "))
+        name = input("Enter Name of Employee : ")
+        desg = input("Enter the Designation : ")
+        sal = int(input("Enter the Salray : "))
+        city = input("Enter the City :")
+        dic = {"ID": eid, "Name": name,"Designation": desg, "Salary": sal, "City": city}
         pickle.dump(dic, f)
         ans = input("Add more?y/n ")
-with open("stud.dat", "rb") as f:
-    try:
-        while True:
-            r = pickle.load(f)
-            if r["Name"][0].lower() == "r" and r["Name"][-1].lower() == "p":
-                print(r)
-    except:
-        pass
+    with open("emp.dat", "rb") as f:
+        try:
+            while True:
+                r = pickle.load(f)
+                if r["Name"][0].lower() == "r" and r["Name"][-1].lower() == "p":
+                    print(r)
+        except:
+            print("NO SUCH DATA FOUND")

caba8aa1a55fd6558b712ab8526e4ce785b82a50
rishi1215 <122065899+rishi1215@users.noreply.github.com>
2023-05-18 20:46:03 +0530

    Add files via upload

---
+++ Q7EMPcode.py
@@ -0,0 +1,36 @@
+def Newemp():
+    import pickle
+    with open("emp.dat", "ab") as f:
+         ans = "y"
+         while ans.lower() == "y":
+            empno = int(input("Enter Employee Number :"))
+            name = input("Enter the Name : ")
+            post = input("Enter the Post : ")
+            sal = int(input("Enter the Salary : "))
+            lst = [empno, name, post, sal]
+            pickle.dump(lst, f)
+            print("Data saved!!")
+            ans = input("Add more?y/n")
+    with open("emp.dat","rb") as f:
+        try:
+            while True:
+                r = pickle.load(f)
+                print(r)
+        except:
+            pass
+def sumsalary(post):
+    import pickle
+    count = 0
+    with open("emp.dat","rb") as f:
+        try:
+            while True:
+                r = pickle.load(f)
+                if r[2] == post:
+                    count += r[3]
+                    print(r[3])
+
+        except:
+            pass
+    print("sum of salaries of employees of the post is ",count)
+Newemp()
+sumsalary("l")
